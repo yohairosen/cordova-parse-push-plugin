9d7b6b2f3982356cf98499c63504de6000df7cc6
Yohai Rosen <yohai.roz@gmail.com>
2016-05-25 13:32:30 +0200

    add auto main file

---
+++ scripts/android_app_name.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+module.exports = function(context) {
+
+    console.log(context);
+
+    var fs = context.requireCordovaModule('fs'),
+        path = context.requireCordovaModule('path');
+
+    var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+
+
+    var manifestFile = path.join(platformRoot, 'AndroidManifest.xml');
+
+    if (fs.existsSync(manifestFile)) {
+
+        fs.readFile(manifestFile, 'utf8', function (err,data) {
+            if (err) {
+                throw new Error('Unable to find AndroidManifest.xml: ' + err);
+            }
+
+            var appClass = 'YOU_APP_CLASS';
+
+            if (data.indexOf(appClass) == -1) {
+
+                var result = data.replace(/<application/g, '<application android:name="' + appClass + '"');
+
+                fs.writeFile(manifestFile, result, 'utf8', function (err) {
+                    if (err) throw new Error('Unable to write into AndroidManifest.xml: ' + err);
+                })
+            }
+        });
+    }
+
+
+};
